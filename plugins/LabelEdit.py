04bfc9136da1b77c969cf2c75bb81449e223b4ad
x64off <120844879+x64off@users.noreply.github.com>
2022-12-24 19:44:59 +0300

    plgn template

--- plugins/LabelEdit.py
+++
@@ -1,8 +0,0 @@
-import uuid
-class LabelEdit:
-    __ID == str(uuid.uuid4())
-    def __init__(self,App):
-        App.Label['text'] = "Label Edit Plugin"
-    @property
-    def ID(self):
-        return self.__ID
